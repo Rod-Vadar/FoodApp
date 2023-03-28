dfe7440bcedb7d6cf969925d8faaec97d2370919
Rod-Vadar <rod643@hotmail.co.uk>
2023-03-28 19:01:27 +0000

    Add background image

--- deliver/settings.py
+++ deliver/settings.py
@@ -149,3 +149,7 @@ LOGIN_REDIRECT_URL = 'dashboard'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
